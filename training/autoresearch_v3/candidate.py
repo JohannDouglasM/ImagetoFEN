2ecabb56fd564be39707f9bd39564399ab2bb9a5
johanndouglasM <johanndmb@gmail.com>
2026-04-28 14:34:38 +0200

    feat: reduce focal gamma 2.0->1.5 to rebalance with label smoothing

--- training/autoresearch_v3/candidate.py
+++ training/autoresearch_v3/candidate.py
@@ -83,7 +83,7 @@ _TYPE_GROUPS = [
 _CLASS_TO_COLOR = [0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 2]
 _CLASS_TO_TYPE = [1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5, 6]
 AUX_FACTOR_LOSS_WEIGHT = 0.3
-FOCAL_GAMMA = 2.0
+FOCAL_GAMMA = 1.5
 LABEL_SMOOTHING = 0.1
 
 IMAGENET_MEAN = np.array([0.485, 0.456, 0.406], dtype=np.float32).reshape(3, 1, 1)
